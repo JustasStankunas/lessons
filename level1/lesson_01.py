4b56832aa9561a9a6ef82964152dc7e5c68e82a7
Justas Stankunas <justas@debian.local>
2017-09-28 20:59:14 +0300

    lesson 1. while loop compare

--- level1/lesson_01.py
+++ level1/lesson_01.py
@@ -3,10 +3,27 @@
 #between 2000 and 3200 (both included).
 #The numbers obtained should be printed in a comma-separated sequence on a single line.
 
+from datetime import datetime
+
+startTime = datetime.now()
 answer = []
 
 for i in range (2000,3201):
     if (i%7==0) and (i%5!=0):
         answer.append(str(i))
 
-print ','.join(answer)
+print (','.join(answer))
+
+print ("\nScript running time: ", datetime.now() - startTime, "\n")
+
+startTime = datetime.now()
+answer = []
+i = 2000
+while i==3200:
+    if (i%7==0) and (i%5!=0):
+        answer.append(str(i))
+    i += 1
+
+print(','.join(ans))
+
+print ("\nScript running time: ", datetime.now() - startTime, "\n")
